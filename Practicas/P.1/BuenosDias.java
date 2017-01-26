8f9d026044bc65b770abc66d6dc8849ac14d23ce
DanielRamiz <danielramiz-95@outlook.com>
2017-01-26 17:12:13 -0600

    Estructura de repositorio

---
+++ Practicas/P.1/BuenosDias.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ public class BuenosDias {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cadena;
+        System.out.println("Introduce un nombre: ");
+        cadena = sc.nextLine();
+        System.out.println("Buenos Días " + cadena);
+    }
+}
+
