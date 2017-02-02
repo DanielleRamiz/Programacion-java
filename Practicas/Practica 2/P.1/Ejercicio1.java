208cece360c997c0c098b88493a846f2ac5f3790
DanielRamiz <danielramiz-95@outlook.com>
2017-02-01 23:56:25 -0600

    Estructura del repositorio

---
+++ Practicas/Practica 2/P.1/Ejercicio1.java
@@ -0,0 +1,28 @@
+public class Ejercicio1 {
+public static void main(String[] args) {
+	byte var_byte =  127; // solo permite 3 cifras
+	short var_short =  32767; // solo permite 5 cifras
+	int var_entera =  147487647; // slo permite 10 cifras
+	long var_long =  854775807; // solo permite 9 cifras
+    char var_char =  2; // depende que tipo de caracter quieras 
+    String var = null;
+    //String var = hola java;
+    
+
+	System.out.println("Variable byte : " + var_byte);
+	System.out.println("Variable short : " + var_short);
+	System.out.println("Variable entera : " + var_entera);
+	System.out.println("Variable long : " + var_long);
+	System.out.println("Variable char : " + var_char);
+	System.out.println (var);
+	//System.out.println ();
+
+
+
+
+}
+}
+
+
+
+
